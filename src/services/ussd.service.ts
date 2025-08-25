a279a70a6a6f57627df1654ed96251de0d2c46df
guglextech <guglex.technologies@gmail.com>
2025-08-25 14:05:54 +0000

    [WIP] +> release after payment

--- src/services/ussd.service.ts
+++ src/services/ussd.service.ts
@@ -358,7 +358,7 @@ export class UssdService {
     const response: any = {
       SessionId: req.SessionId,
       Type: HbEnums.ADDTOCART,
-      Message: `Payment request for GHS ${total} has been submitted. Kindly approve the MOMO prompt. If no prompt, Dial *170# select 6) My Wallet 3) My Approvals, enter your PIN and approve the payment.`,
+      Message: `Payment request for GHS ${total} has been submitted. Kindly approve the MOMO prompt. If no prompt, Dial *170# select 6) My Wallet 3) My Approvals`,
       Item: new CheckOutItem(state.service, 1, total),
       Label: "Payment Request Submitted",
       DataType: HbEnums.DATATYPE_DISPLAY,
